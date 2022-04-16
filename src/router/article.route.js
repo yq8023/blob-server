55ebbe70100fd431ca4a5124e31a97181b6b4f10
杨清 <yangqing.0106@bytedance.com>
2022-04-16 20:01:42 +0800

    feat: 支持文章图片上传

--- src/router/article.route.js
+++ src/router/article.route.js
@@ -7,12 +7,14 @@ const {
   list,
   deleteArticle,
 } = require("../controller/article.controller");
+const { ImgUpload } = require("../middleware/common/upload.middleware");
 
 const articleRouter = new Router({ prefix: "/article" });
 
 articleRouter.get("/list", auth, list);
 
 articleRouter.post("/add", auth, addArticle);
+articleRouter.post("/img_upload", auth, ImgUpload);
 articleRouter.post("/delete", auth, deleteArticle);
 
 module.exports = articleRouter;
